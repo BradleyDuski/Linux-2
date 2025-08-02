a2c6a5843599f3a205d8e9d92174678ea382281f
Bradley Duski <Bradley.Duski@sdgku.edu>
2025-08-02 01:20:10 -0500

    Add files via upload

---
+++ app/utils/delete_task.py
@@ -0,0 +1,3 @@
+# the user must specify the ID of the task they wish to delete
+#The function in requests to issue a DELETE request is literally delete
+# response = requests.delete(url)
---
+++ app/utils/update_task.py
@@ -0,0 +1,4 @@
+
+# the suer must be able to select the id of the task they wish to update
+# The function in requests to issue a PUT request is literally put
+# response = requests.put(URL, json=your_data)
